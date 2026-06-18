bb32717b248808377e60ac33e1970a2c93db88dd
jose <jlfp15@gmail.com>
2026-06-18 17:25:20 -0300

    fix

--- eww_start.sh
+++ eww_start.sh
@@ -9,11 +9,12 @@ export PATH="$HOME/.cargo/bin:$HOME/.local/bin:$PATH"
 # 3. Iniciar el demonio con variables de entorno para Wayland
 export GDK_BACKEND=wayland
 export GTK_THEME=Adwaita:dark
-/home/jose/eww/target/release/eww daemon
+/home/jose/eww/target/release/eww daemon &
 
 # 4. Esperar a que el demonio cargue
 sleep 1
 
-# 6. Abrir la barra
+# 6. Abrir la barra y el dashboard (conky)
 /home/jose/eww/target/release/eww open eww-bar
+/home/jose/eww/target/release/eww open solar-dashboard
 
