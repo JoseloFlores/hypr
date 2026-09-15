dc4b7838cc76e69eb863e9155114b274c947fffe
JoseloFlores <jlfp15@gmail.com>
2026-09-15 16:08:40 -0300

    fix greetd: usar start-hyprland en --cmd tuigreet

--- install-scripts/60-greetd.sh
+++ install-scripts/60-greetd.sh
@@ -20,7 +20,7 @@ cat > /etc/greetd/config.toml <<'EOF'
 [terminal]
 vt = 1
 [default_session]
-command = "/usr/bin/tuigreet --time --remember --remember-session --asterisks --sessions /usr/share/wayland-sessions --cmd Hyprland"
+command = "/usr/bin/tuigreet --time --remember --remember-session --asterisks --sessions /usr/share/wayland-sessions --cmd start-hyprland"
 user = "_greetd"
 EOF
 
